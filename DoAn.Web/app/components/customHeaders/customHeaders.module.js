78b3248ed28c3636f5f26c5b77c50f31579ff847
Hau Le <lethehau90.vn@gmail.com>
2017-04-17 11:25:57 +0700

    change

---
+++ DoAn.Web/app/components/customHeaders/customHeaders.module.js
@@ -0,0 +1,22 @@
+﻿/// <reference path="\Assets/admin/libs/angular/angular.js" />
+(function () {
+    angular.module('doan.customHeaders', ['doan.common']).config(config)
+
+    config.$inject = ['$stateProvider', '$urlRouterProvider']
+
+    function config($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('customheader', {
+                url: '/customheader',
+                parent: 'base',
+                templateUrl: '/app/components/customHeaders/customHeaderListView.html',
+                controller: 'customHeaderListController'
+            })
+            .state('customheaderedit', {
+                url: '/customheaderedit/:id',
+                parent: 'base',
+                templateUrl: '/app/components/customHeaders/customHeaderEditView.html',
+                controller: 'customHeaderEditController'
+            });
+    }
+})();
