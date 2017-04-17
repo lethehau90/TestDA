8794a0cba1c2c9189a9b1617d1aec15b61c78a2b
Hau Le <lethehau90.vn@gmail.com>
2017-04-17 11:38:56 +0700

    change

--- DoAn.Web/app/components/customHeaders/customHeaderEditController.js
+++ DoAn.Web/app/components/customHeaders/customHeaderEditController.js
@@ -1,1 +1,35 @@
-﻿
+﻿/// <reference path="\Assets/admin/libs/angular/angular.js" />
+(function (app) {
+    app.controller('customHeaderEditController', customHeaderEditController)
+
+    customHeaderEditController.$inject = ['$scope', '$state', '$stateParams', 'apiService', 'notificationService']
+
+    function customHeaderEditController($scope, $state, $stateParams, apiService, notificationService) {
+
+        $scope.customHeader = []
+
+        $scope.getDetailCustomHeader = getDetailCustomHeader
+        $scope.EditCustomHeader = EditCustomHeader
+
+        function EditCustomHeader() {
+            apiService.put('/api/customheader/update', $scope.customHeader, function (result) {
+                notificationService.displaySuccess('Cập nhật thành công ' + result.data.Name)
+                $state.go('customheader')
+            }, function (error) {
+                console.log('lỗi')
+            })
+        }
+
+        function getDetailCustomHeader() {
+            apiService.get('/api/customheader/getbyid/' + $stateParams.id, null, function (result) {
+                $scope.customHeader = result.data
+            }, function (error) {
+                console.log('lỗi')
+            })
+        }
+
+        getDetailCustomHeader()
+
+    }
+
+})(angular.module('doan.customHeaders'))
